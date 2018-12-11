ee4b320fa84f31d1a9e32c3b153b4e10de8b3fbd
Paweł <ppierzc@gmail.com>
2018-12-11 12:45:52 +0100

    [CW4] AIC func

--- cw4_utils.py
+++ cw4_utils.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-\mathrm{AIC}(p)=\mathrm{ln}(\det({V}))+2\frac{pk^2}{N}
+def coeffs(signal, low, high):
+	return [ mult_AR(signal, order, 1) for order in range(low, high + 1) ]
 
 def akaike(p, V, shape):
 	return np.log(np.linalg.det(V)) + 2 * p * shape[0] ** 2 / shape[1]
+
+def get_aic_func(signal, low, high):
+	cfs = coeffs(signal, low, high)
+	aic = [akaike(p + 1, cf, signal.shape) for p, cf in enumerate(cfs)]
 
