92afdc74af7f13549fa418a03523a672c8ce7b8c
PaziewskiCezary <33704494+PaziewskiCezary@users.noreply.github.com>
2018-12-13 14:12:38 +0100

    Fixed functions and added new one

--- cw4_utils.py
+++ cw4_utils.py
@@ -2,12 +2,25 @@ import numpy as np
 import PracowniaEEG.mtmvar as mtmvar
 
 def coeffs(signal, low, high):
-	return [ mtmvar.mult_AR(signal, order, 1) for order in range(low, high + 1) ]
-
-def akaike(p, V, shape):
-	return np.log(np.linalg.det(V)) + 2 * p * shape[0] ** 2 / shape[1]
+  return [ mtmvar.mult_AR(signal, order, 1) for order in range(low, high + 1) ]
 
 def get_aic_func(signal, low, high):
-	cfs = coeffs(signal, low, high)
-	aic = [akaike(p + 1, cf, signal.shape) for p, cf in enumerate(cfs)]
-	return np.array(aic)
+  cfs = coeffs(signal, low, high)
+  aic = [akaike(len(params), covars, signal.shape) for params, covars in cfs]
+  return aic
+
+def akaike(p, V, shape):
+  """
+  p: order
+  V: covars matrix
+  shape: input shape
+  """
+
+  return np.log(np.linalg.det(V)) + 2 * p * shape[0] * shape[0] / shape[1]
+
+def best_akaike(signal, low, high):
+  akaike_scores = get_aic_func(s, 1, 20)
+  best_index = akaike_scores.index(min(akaike_scores))
+  order = low + best_index
+  params, covars = mtmvar.mult_AR(signal, order, 1)
+  return order, params, covars
